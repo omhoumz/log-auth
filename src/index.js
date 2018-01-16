1969ac97a9e3c347a53687e660765c3824f52e73
Omar Houmz <omarhoumz@gmail.com>
2018-01-16 21:49:20 +0000

    Initial clean skeleton

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+import './css/index.css';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
